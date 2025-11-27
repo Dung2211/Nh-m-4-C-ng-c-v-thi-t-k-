e892fba79c3e7a29c5746f99009a12b045595adc
Dungdeptrai <hophuongdung2004@gmail.com>
2025-11-27 21:44:01 +0700

    6.Dũng update 27/11

--- backend/dangnhap.js
+++ backend/dangnhap.js
@@ -5,15 +5,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const USER = {
-  username: "dungdeptrai",
-  password: "123456",
-};
+const USER = [
+  { username: "admin", password: "dungdeptrai" },
+  { username: "dung", password: "111" },
+  { username: "khanh", password: "111" },
+  { username: "linh", password: "111" },
+  { username: "tdung", password: "111" },
+  { username: "rin", password: "111" },
+  { username: "mkhanh", password: "111" },
+];
+// const USER = {
+//   username: "admin",
+//   password: "123"
+// };
 
+// app.post("/login", (req, res) => {
+//   const { username, password } = req.body;
+
+//   if (username === USER.username && password === USER.password) {
+//     return res.json({ success: true, message: "Dang nhap thanh cong!" });
+//   } else {
+//     return res.json({
+//       success: false,
+//       message: "Sai tai khoan hoac mat khau!",
+//     });
+//   }
+// });
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  if (username === USER.username && password === USER.password) {
+  const user = USER.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (user) {
     return res.json({ success: true, message: "Dang nhap thanh cong!" });
   } else {
     return res.json({
